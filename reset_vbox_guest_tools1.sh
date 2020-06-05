43e45d554645805c5a0d2aa370f7c209a5c1ea02
Zarcolio <registered@quicknet.nl>
2020-06-05 18:20:26 +0200

    Add files via upload

---
+++ reset_vbox_guest_tools1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt update
+apt upgrade -y
+apt --purge remove virtualbox-guest-x11
+apt clean
+echo ""
+read -p "Press [Enter] key to shutdown or CTRL-C to cancel"
+shutdown --poweroff  now
---
+++ reset_vbox_guest_tools2.sh
@@ -0,0 +1,4 @@
+apt install virtualbox-guest-x11
+echo ""
+read -p "Press [Enter] key to shutdown or CTRL-C to cancel"
+shutdown --poweroff now
